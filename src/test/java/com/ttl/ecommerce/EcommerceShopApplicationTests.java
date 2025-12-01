2abba29f9c9af598994108e9cd75decccb28c9c7
Tan Le <tanh.le@tx-tech.com>
2025-12-01 13:48:15 +0700

    init

---
+++ src/test/java/com/ttl/ecommerce/EcommerceShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttl.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
