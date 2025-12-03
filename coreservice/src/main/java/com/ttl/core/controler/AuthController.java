069d1e5d33626ca3351694001fbb110d49a02520
tuv.tran <tuv.tran@tx-tech.com>
2025-12-03 15:32:50 +0700

    refactor response core

--- coreservice/src/main/java/com/ttl/core/controler/AuthController.java
+++ coreservice/src/main/java/com/ttl/core/controler/AuthController.java
@@ -1,6 +1,20 @@
 package com.ttl.core.controler;
 
-import com.ttl.common.constant.ApiResponse;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.WebUtils;
+
 import com.ttl.common.constant.ITag;
 import com.ttl.common.constant.ITagCode;
 import com.ttl.common.exception.BussinessException;
@@ -9,23 +23,15 @@ import com.ttl.common.utilities.CoreUtils;
 import com.ttl.core.config.OtpGeneration;
 import com.ttl.core.request.LoginRequest;
 import com.ttl.core.request.RegisterRequest;
+import com.ttl.core.response.ErrorMessage;
 import com.ttl.core.response.LoginResponse;
 import com.ttl.core.service.AuthService;
 import com.ttl.core.service.OtpEmailService;
 import com.ttl.core.service.OtpRedisService;
+
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.WebUtils;
-
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -47,7 +53,7 @@ public class AuthController {
 		mvOtpRedisService = pOtpRedisService;
 	}
 	@PostMapping("/login")
-	public ResponseEntity<ApiResponse<?>> login(@RequestBody LoginRequest pLoginRequest, HttpServletRequest pHttpRequest) throws BussinessException, InterruptedException, ExecutionException{
+	public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest pLoginRequest, HttpServletRequest pHttpRequest) throws BussinessException, InterruptedException, ExecutionException{
 		LoginResponse lvLoginResponse = mvAuthService.login(pLoginRequest, pHttpRequest).get();
 		ResponseCookie lvResponseCookie = ResponseCookie.from(ITag.REFRESH_TOKEN, lvLoginResponse.getRefreshToken())
 				.httpOnly(true)
@@ -58,10 +64,10 @@ public class AuthController {
 				.build();
 		return ResponseEntity.ok()
 				.header(HttpHeaders.SET_COOKIE, lvResponseCookie.toString())
-				.body(ApiResponse.success("Đăng nhâp thành công !", lvLoginResponse));
+				.body(lvLoginResponse);
 	}
 	@PostMapping("/refresh")
-	public ResponseEntity<ApiResponse<?>> refreshToken(
+	public ResponseEntity<?> refreshToken(
 //			@CookieValue(name = ITag.REFRESH_TOKEN, required = false) String pRefreshToken, 
 			HttpServletRequest request) throws UserNotFoundException {
 	    Cookie cookie = WebUtils.getCookie(request, ITag.REFRESH_TOKEN);
@@ -69,14 +75,15 @@ public class AuthController {
 	    if (cookie == null) {
 	        System.out.println("❌ Không có refresh_token cookie");
 	        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-	            .body(ApiResponse.error("No refresh token cookie", ITagCode.UNKNOWN_ERROR, getClass()));
+	            .body(new ErrorMessage("No refresh token cookie", ITagCode.UNKNOWN_ERROR));
 	    }
 	    String lvRefreshToken = cookie.getValue();
 		String lvAccessToken = mvAuthService.retrieveTokenByRefreshToken(lvRefreshToken);
 		if(!CoreUtils.isNullStr(lvAccessToken)) {
-			return ResponseEntity.ok(ApiResponse.success("Refresh token successfully!", lvAccessToken));
+			return ResponseEntity.ok(lvAccessToken);
 		}
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("Invalid Refresh Token", ITagCode.UNKNOWN_ERROR, getClass()) );
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+				.body(new ErrorMessage("Invalid Refresh Token", ITagCode.UNKNOWN_ERROR));
 	}
 	
 	@PostMapping("/logout")
@@ -92,7 +99,7 @@ public class AuthController {
         // Xóa refresh token trong cookie
 		Cookie lvCookie = mvAuthService.logout(pHttpReq);
         pHttpRes.addCookie(lvCookie);
-        return ResponseEntity.ok(Map.of("message", "Logged out successfully"));
+        return ResponseEntity.ok("Logged out successfully");
     }
 	/*
 	 * register : frontend send API /sendotp , send API /sendotpregister 
@@ -105,29 +112,30 @@ public class AuthController {
 //	}
 	
 	@PostMapping("/sendotp")
-	public ResponseEntity<ApiResponse<?>> sendOtp(@RequestParam String pOtpType ,@RequestParam String pToEmail){
+	public ResponseEntity<?> sendOtp(@RequestParam String pOtpType ,@RequestParam String pToEmail){
 		String lvOtp = mvOtpGeneration.generationOtp(6);
 		mvEmailService.sendOtpEmail(pToEmail, lvOtp);
 		mvOtpRedisService.saveOtp(pOtpType, pToEmail, lvOtp);
-		return ResponseEntity.ok(ApiResponse.success("OTP sent to: " + pToEmail, null));
+		  return ResponseEntity.ok(pToEmail);
 	}
 	@PostMapping("/sendotpregister")
-	public ResponseEntity<ApiResponse<?>> sendOtpRegister(@RequestBody RegisterRequest pUser){
+	public ResponseEntity<?> sendOtpRegister(@RequestBody RegisterRequest pUser){
 		String lvOtp = mvOtpGeneration.generationOtp(6);
 		mvEmailService.sendOtpEmail(pUser.getEmail(), lvOtp);
 		mvOtpRedisService.saveOtp(ITag.OTP_REGISTER, pUser.getEmail(), lvOtp);
 		mvOtpRedisService.saveTempRegister(pUser);
-		return ResponseEntity.ok(ApiResponse.success("OTP sent to: " + pUser.getEmail(), null));
+		 return ResponseEntity.ok(pUser.getEmail());
 	}
 	@PostMapping("/verifyotp")
-	public ResponseEntity<ApiResponse<?>> verifyOtp(@RequestParam String pOtpType, @RequestParam String pEmail, @RequestParam String pOtp){
+	public ResponseEntity<?> verifyOtp(@RequestParam String pOtpType, @RequestParam String pEmail, @RequestParam String pOtp){
 		boolean lvIsOtpSusscess = mvOtpRedisService.verifyOtp(pOtpType, pEmail, pOtp);
 		boolean lvIsUpdate = mvAuthService.verifyRegister(pEmail);
 		if(lvIsOtpSusscess && lvIsUpdate) {
 			mvOtpRedisService.deleteOtp(pOtpType, pEmail);
-			return ResponseEntity.ok(ApiResponse.success("Otp verify susscessfully !", pOtp));
+			return ResponseEntity.ok(pOtp);
 		}else {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse.error("Invalid or expired OTP", ITagCode.OTP_INVALID, getClass()));
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+					.body(new ErrorMessage("Invalid or expired OTP", ITagCode.OTP_INVALID));
 		}
 	}
 	
